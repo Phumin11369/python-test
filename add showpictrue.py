df42b09a566b528e9e4877c926762626482caabb
Phumin11369 <phumin.k67@rsu.ac.th>
2024-07-23 09:00:58 +0700

    Add files via upload

---
+++ add showpictrue.py
@@ -0,0 +1,12 @@
+#string message ' "" """
+#number integer float
+#boolean True False
+print(-1)
+print(1)
+print(1.01)
+print(5+5)
+print("5" + "5")
+print(False)
+print(True)
+print("Test \n" + "Test")#ขึ้นบรรทัดใหม่
+print("test \t" + "test")#เว้นวรรค
